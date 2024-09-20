208969d8c725e1494c80de25c7fd914c7b376366
Benjaminlucky <42000766+Benjaminlucky@users.noreply.github.com>
2024-09-20 10:19:51 +0100

    what makes us different updated to be responsive

--- client/src/components/different/Different.jsx
+++ client/src/components/different/Different.jsx
@@ -4,10 +4,10 @@ import { difference } from "../../data";
 function Different() {
   return (
     <div className="innerWrapper w-4/5 py-20 flex justify-center mx-auto">
-      <div className="sectionContent w-full gap-5 py-20 flex items-center">
-        <div className="left flex-1">
-          <div className="title pr-16">
-            <h1 className="font-extrabold text-6xl leading-normal">
+      <div className="sectionContent w-full gap-5 md:gap-20 md:py-20 lg:py-20 flex flex-col md:flex-col lg:flex-row items-center">
+        <div className="left w-full flex-1 ">
+          <div className="title  lg:pr-16 flex justify-center text-center">
+            <h1 className="font-extrabold text-2xl text-center md:text-6xl lg:text-6xl lg:leading-normal">
               What Makes Alliance-Fx Market Different?
             </h1>
           </div>
