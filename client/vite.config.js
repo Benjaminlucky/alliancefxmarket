591b3eaf5eb800e1596b92df39fa0683eb55035f
Benjaminlucky <42000766+Benjaminlucky@users.noreply.github.com>
2024-09-27 16:47:38 +0100

    body on signup black removed

--- client/vite.config.js
+++ client/vite.config.js
@@ -12,4 +12,26 @@ export default defineConfig({
       extensions: [".js", ".jsx"], // Enable JSX syntax in .js files
     }),
   ],
+  build: {
+    target: "esnext", // Ensure modern JS is used
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return id
+              .toString()
+              .split("node_modules/")[1]
+              .split("/")[0]
+              .toString();
+          }
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1000, // Adjust chunk size warning if necessary
+  },
+  server: {
+    mimeTypes: {
+      "application/javascript": ["js", "jsx"], // Ensure JS and JSX are served correctly
+    },
+  },
 });
